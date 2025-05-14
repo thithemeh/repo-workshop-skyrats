d7e33111dbea64b5b19e0289aa7ac259f812b668
ThiagoM <thiagomartins2710@gmail.com>
2025-05-13 23:26:16 -0300

    :tada: Primeiro commit, com adição dos arquivos iniciais, da logo e do README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Repositório para EP de Git/Github
+
+![logo](Skyrats.png)
+
+Este repositório será utilizado para a realização do EP sobre Git e Github da Skyrats.
+
+Para isso, serão realizadas operações de commit e push com arquivos escritos em linguagem
+Python, os quais serão especificamente para a execução de diversas operações matemáticas.
+
+Autor: Thiago Martins (Catatau)
---
+++ ep.py
@@ -0,0 +1,47 @@
+##### EP - GIT ###
+# Ao implementar uma função, dê commit no github usando os comandos vistos no workshop!
+# Implemente uma função de cada vez, e dê commit em uma função por vez.
+# NOME:
+
+
+def minimo(arr):
+    # retorna o menor valor do array
+    return min(arr)
+
+def maximo(arr):
+    # retorna o maior valor do array
+    return
+
+def meio(arr):
+    # retorna o valor exatamente ao meio do array
+    return
+
+def media(arr):
+    # retorna a media dos valores do array
+    return
+
+def moda(arr):
+    # retorna o valor que mais se repete no array
+    return
+
+def soma(arr, n = 1):
+    # soma n a todos os valores do array e retorna o novo array
+    # exemplo
+    # entrada: arr = [1, 1, 1], n = 5
+    # saida: arr = [6, 6, 6]
+    return
+
+def subtracao(arr, n):
+    # subtrai n em todos os valores do array e retorna o novo array
+    # exemplo
+    # entrada: arr = [1, 1, 1], n = 5
+    # saida: arr = [-4, -4, -4]
+    return
+
+def soma_arr(arr1, arr2):
+    # soma de dois arrays elemento-a-elemento e retorna
+    return
+
+def produto_interno(arr1, arr2):
+    # retorna o produto interno entre dois vetores
+    return
